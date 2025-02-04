163e33976a14a20ad79ada6a4dc473d631aff868
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2025-02-04 16:59:57 +0100

    none cleanup and corrected places to call scripts and scripts enhancements

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
 
+# Check if node_modules exists, if not call mkdir -p
+if [ ! -d "/workspace/node_modules" ]; then
+  sudo mkdir -p /workspace/node_modules/.pnpm
+fi
+
 # Change ownership of the node_modules directory
 sudo chown -R $USER:$USER /workspace/node_modules
+sudo chown -R $USER:$USER /workspace/node_modules/.pnpm
+# Give all permissions to the node_modules directory
+chmod -R 0777 /workspace/node_modules
+cd /workspace
+# Install packages using pnpm
+pnpm install
 
-# Execute the container's main process (what's set as CMD in the Dockerfile)
-exec "$@"
+# Check if we need to approve builds
+sudo mv /usr/local/bin/approve-builds.exp /workspace
+sudo chown $USER:$USER /workspace/approve-builds.exp
+expect ./approve-builds.exp
